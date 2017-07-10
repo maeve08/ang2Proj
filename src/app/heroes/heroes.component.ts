a569f3a0fb4647b53cba6a6963bb356727349941
Eva Plasencia <eva.plasencia@auspost.com.au>
2017-07-10 18:18:33 +1000

    Updating css to sass.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,7 +7,7 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: ['./heroes.component.scss'],
   providers: []
 })
 export class HeroesComponent implements OnInit {
